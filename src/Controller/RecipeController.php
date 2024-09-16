f3a43e469188a6e95f2b8acda98d9e65f6dd61b5
Didier Maba <deus_maba@yahoo.fr>
2024-09-16 18:51:43 +0200

    recipe entity & controller

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,18 +14,111 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recette', name: 'recipe.index')]
-    public function index(Request $request): Response
+    #[Route('/recettes', name: 'recipe.index')]
+    public function index(Request $request, RecipeRepository $repository): Response
     {
-        return new Response('Recettes');
+        $recipes = $repository->findAll();
+        // dd($recipes);
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes
+        ]);
     }
 
-    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements:['id'=>'\d+', 'slug'=>'[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id): Response
+    #[Route('/duration', name: 'recipe.duration')]
+    public function duration(Request $request, RecipeRepository $repository): Response
     {
-        // return $this->render('recipe/index.html.twig', [
-        
-        // ]);
-        return new Response('Recette :' . $slug);
+        $recipes = $repository->findWithDurationLowerThan(16);
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes
+        ]);
+    }
+
+    // route créée en fx de besoin grace au query builder ds la class repository 
+    #[Route('/modify', name: 'recipe.modify')]
+    public function modify(Request $request, RecipeRepository $repository, EntityManagerInterface $em): Response
+    {
+        $recipes = $repository->findWithDurationLowerThan(16);
+        $recipes[0]->setTitle('Pates bolognaise');
+        $em->flush();
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes
+        ]);
+    }
+
+    #[Route('/crea', name: 'recipe.crea')]
+    public function crea(Request $request, RecipeRepository $repository, EntityManagerInterface $em): Response
+    {
+        $recipes = $repository->findWithDurationLowerThan(16);
+
+        $recipe = new Recipe();
+        $recipe->setTitle('barbe à papa')
+            ->setSlug('barbe-papa')
+            ->setContent('Pause repas à l\'italienne avec ces spaghetti à la bolognaise. Un concentré de saveurs composé d’une viande de bœuf hachée agrémentée de tomates, d\'oignons et de basilic. Prêt en quelques minutes au micro-ondes, ce plat express donne une note chaleureuse à vos déjeuners ou dîners solo.||1 part\n\nPause repas à l\'italienne avec ces spaghetti à la bolognaise. Un concentré de saveurs composé d’une viande de bœuf hachée agrémentée de tomates, d\'oignons et de basilic. Prêt en quelques minutes au micro-ondes, ce plat express donne une note chaleureuse à vos déjeuners ou dîners solo.')
+            ->setDuration(5)
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->setUpdatedAt(new \DateTimeImmutable());
+        $em->persist($recipe); // pr suivre l'objet crée
+        $em->flush(); // pr memoriser les infos en bd
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes
+        ]);
+    }
+
+    #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function show(Request $request, string $slug, int $id, RecipeRepository $repository): Response
+    {
+        $recipe = $repository->find($id);
+        if ($recipe->getSlug() !== $slug) {
+            return $this->redirectToRoute('recipe.show', ['slug' => $recipe->getSlug(), 'id' => $recipe->getId()]);
+        }
+
+        return $this->render('recipe/show.html.twig', [
+            'recipe' => $recipe,
+        ]);
+    }
+    #[Route('/recettes/{id}/edit', name: 'recipe.edit', methods: ['GET', 'POST'])]
+    public function edit(Recipe $recipe, Request $request, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $recipe->setUpdatedAt(new \DateTimeImmutable());
+            $em->flush();
+            $this->addFlash('success', 'la recette a bien été modifiée');
+            return $this->redirectToRoute('recipe.index');
+        }
+
+        return $this->render('recipe/edit.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/recettes/create', name: 'recipe.create')]
+    public function create(Request $request, EntityManagerInterface $em)
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $recipe->setCreatedAt(new \DateTimeImmutable());
+            // $recipe->setUpdatedAt(new \DateTimeImmutable());
+            $em->persist($recipe);
+            $em->flush();
+            $this->addFlash('success', 'Votre recette a bien été enregistré');
+            return $this->redirectToRoute('recipe.index');
+        }
+        return $this->render('recipe/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/recettes/{id}/delete', name: 'recipe.delete', methods: ['DELETE'])]
+    public function remove(Recipe $recipe, EntityManagerInterface $em) {
+
+        $em->remove($recipe);
+        $em->flush();
+        $this->addFlash('success', 'La recette a bien été supprimée');
+        return $this->redirectToRoute('recipe.index');
     }
 }
